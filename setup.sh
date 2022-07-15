3e976b132d852f1bb0baf3a95a091e7bfdc11ae3
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-07-15 21:14:12 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,71 +1,21 @@
-#!/bin/bash
+#!/usr/bin/env bash
 echo ""
-
-## Script name
 SCRIPT_NAME=zzmysqldump
 
-## Install directory
-WORKING_DIR_ORIGINAL="$(pwd)"
-INSTALL_DIR_PARENT="/usr/local/turbolab.it/"
-INSTALL_DIR=${INSTALL_DIR_PARENT}${SCRIPT_NAME}/
+## bash-fx
+sudo apt update && sudo apt install curl -y
+curl -s https://raw.githubusercontent.com/TurboLabIt/bash-fx/master/setup.sh?$(date +%s) | sudo bash
+source /usr/local/turbolab.it/bash-fx/bash-fx.sh
+## bash-fx is ready
 
-## /etc/ config directory
-mkdir -p "/etc/turbolab.it/"
+sudo apt install git mysql-client p7zip-full -y
 
-## Pre-requisites
-if [ ! -d "$INSTALL_DIR" ] && [ -f "/etc/redhat-release" ]; then
+sudo bash /usr/local/turbolab.it/bash-fx/setup/begin.sh ${SCRIPT_NAME}
+fxLinkBin ${INSTALL_DIR}${SCRIPT_NAME}.sh
+fxLinkBin ${INSTALL_DIR}zzmysqlimp.sh
 
-  yum clean all
-  yum install epel-release -y
-  yum clean all
-  yum install git mysql-community-client p7zip -y
-  
-elif [ ! -d "$INSTALL_DIR" ]; then
-
-  apt update && apt install git mysql-client p7zip-full -y
-  
-fi
-
-## Install/update
-echo ""
-if [ ! -d "$INSTALL_DIR" ]; then
-
-  echo "Installing..."
-  echo "-------------"
-  mkdir -p "$INSTALL_DIR_PARENT"
-  cd "$INSTALL_DIR_PARENT"
-  git clone https://github.com/TurboLabIt/${SCRIPT_NAME}.git
-  
-else
-
-  echo "Updating..."
-  echo "----------"
-  
-fi
-
-## Fetch & pull new code
-cd "$INSTALL_DIR"
-git fetch origin
-git merge FETCH_HEAD
-
-## Symlink (globally-available zzmysqldump command)
-if [ ! -e "/usr/bin/${SCRIPT_NAME}" ]; then
-  ln -s ${INSTALL_DIR}${SCRIPT_NAME}.sh /usr/bin/${SCRIPT_NAME}
-fi
-
-if [ ! -e "/usr/bin/zzmysqlimp" ]; then
-  ln -s ${INSTALL_DIR}zzmysqlimp.sh /usr/bin/zzmysqlimp
-fi
-
-## Copy the cron job
 if [ ! -f "/etc/cron.d/zzmysqldump" ]; then
-  cp "${INSTALL_DIR}cron" "/etc/cron.d/zzmysqldump"
+  sudo cp "${INSTALL_DIR}cron" "/etc/cron.d/zzmysqldump"
 fi
 
-## Restore working directory
-cd $WORKING_DIR_ORIGINAL
-
-echo ""
-echo "Setup completed!"
-echo "----------------"
-echo "See https://github.com/TurboLabIt/${SCRIPT_NAME} for the quickstart guide."
+sudo bash /usr/local/turbolab.it/bash-fx/setup/end.sh ${SCRIPT_NAME}
