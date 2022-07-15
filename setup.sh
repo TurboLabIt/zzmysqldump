dadb3751528cc96b4578d3bfdd0d88da6caeab2c
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-07-15 21:31:01 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,7 +8,7 @@ curl -s https://raw.githubusercontent.com/TurboLabIt/bash-fx/master/setup.sh?$(d
 source /usr/local/turbolab.it/bash-fx/bash-fx.sh
 ## bash-fx is ready
 
-sudo bash /usr/local/turbolab.it/bash-fx/setup/begin.sh ${SCRIPT_NAME}
+sudo bash /usr/local/turbolab.it/bash-fx/setup/start.sh ${SCRIPT_NAME}
 sudo apt install git mysql-client p7zip-full -y
 fxLinkBin ${INSTALL_DIR}${SCRIPT_NAME}.sh
 fxLinkBin ${INSTALL_DIR}zzmysqlimp.sh
@@ -17,4 +17,4 @@ if [ ! -f "/etc/cron.d/zzmysqldump" ]; then
   sudo cp "${INSTALL_DIR}cron" "/etc/cron.d/zzmysqldump"
 fi
 
-sudo bash /usr/local/turbolab.it/bash-fx/setup/end.sh ${SCRIPT_NAME}
+sudo bash /usr/local/turbolab.it/bash-fx/setup/the-end.sh ${SCRIPT_NAME}
