059380f7c09bafca17e27ec68d1b5c9ca60c1717
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2022-12-17 22:42:28 +0100

    Update zzmysqlimp.sh
    closes #3

--- zzmysqlimp.sh
+++ zzmysqlimp.sh
@@ -1,55 +1,73 @@
 #!/usr/bin/env bash
-echo ""
 
-## Script name
 SCRIPT_NAME=zzmysqlimp
-
 source "/usr/local/turbolab.it/zzmysqldump/base.sh"
 
 ## Checking input file
 DUMP_FULLPATH=$(readlink -f "$1")
-
 if [ -z "$DUMP_FULLPATH" ] || [ ! -f "$DUMP_FULLPATH" ]; then
 
-  echo ""
-  echo "vvvvvvvvvvvvvvvvvvvv"
-  echo "Catastrophic error!!"
-  echo "^^^^^^^^^^^^^^^^^^^^"
-  echo "Dump to import not found"
-  echo "[X] $DUMP_FULLPATH"
-
-  zzmysqldumpPrintEndFooter
-  exit
-
+  fxCatastrophicError "Dump to import not found!
+  🕳 $DUMP_FULLPATH
+  "
 fi
 
+
+fxTitle "Extracting..."
 DUMP_DIR=$(dirname "$DUMP_FULLPATH")/
 DUMPFILE_FULLPATH=${DUMP_FULLPATH%.7z}
 
-echo ""
-echo "Un-7zipping the file"
-echo "--------------------"
-echo ${1}
-echo ""
+echo "Directory:    ##${DUMP_DIR}##"
+echo "Filename:     ##${DUMPFILE_FULLPATH}##"
 
 if [ ! -f "$DUMPFILE_FULLPATH" ] || [ "$(find "$DUMPFILE_FULLPATH" -mmin +3600)" ]; then
-
   7za e "${1}" -o"${DUMP_DIR}" -y
-  
 else
-
-  echo "The extracted file exists and was created today. Skipping the un-7zipping!"
-  
+  fxInfo "The extracted file exists and was created today. Skipping the un-7zipping!"
 fi
 
-echo ""
-echo "Importing the extracted dump"
-echo "----------------------------"
+fxTitle "Listing..."
+ls -lh "${DUMP_DIR}" | grep $(basename ${DUMPFILE_FULLPATH})
 
-echo ${DUMPFILE_FULLPATH}
-echo ""
+
+fxTitle "Changing database name..."
+
+function sedReplace()
+{
+  sed -i "s|${REPLACE_REGEX}|${REPLACE_WITH_TEXT}|g" "${DUMPFILE_FULLPATH}" --regexp-extended
+  fxOK "Done"
+}
+
+if [ ! -z "$2" ]; then
+
+  fxInfo "OK, the new database name is going to be named #$2#"
+  echo ""
+  
+  REPLACE_REGEX='^-- Current Database: `.+`'
+  REPLACE_WITH_TEXT="--🦎 Database name replaced to ##$2## with https://github.com/TurboLabIt/zzmysqldump"
+  sed -i "s|${REPLACE_REGEX}|&\n${REPLACE_WITH_TEXT}|g" "${DUMPFILE_FULLPATH}" --regexp-extended
+  fxOK "🦎 Note added"
+  
+  REPLACE_REGEX='^/\*!40000 DROP DATABASE IF EXISTS `.+`\*/;'
+  REPLACE_WITH_TEXT="/\\*!40000 DROP DATABASE IF EXISTS \`$2\`\\*/;"  
+  sedReplace "$REPLACE_REGEX" "$REPLACE_WITH_TEXT"
+  
+  REPLACE_REGEX='^CREATE DATABASE /\*!32312 IF NOT EXISTS\*/ `.+`'
+  REPLACE_WITH_TEXT="CREATE DATABASE /\\*!32312 IF NOT EXISTS\\*/ \`$2\`"  
+  sedReplace "$REPLACE_REGEX" "$REPLACE_WITH_TEXT"
+  
+  REPLACE_REGEX='^USE `.+`;'
+  REPLACE_WITH_TEXT="USE \`$2\`;"  
+  sedReplace "$REPLACE_REGEX" "$REPLACE_WITH_TEXT"
+
+else
+
+  fxInfo "New database name not provided. Using the same name provided by the dump"
+fi
+
+
+fxTitle "Importing the extracted dump..."
 mysql -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" -h "$MYSQL_HOST" < "$DUMPFILE_FULLPATH"
 
 
-zzmysqldumpPrintEndFooter
-
+fxEndFooter
