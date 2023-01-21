57fff7bd1b1d88afd3363999ae49e849f703d088
Dr. Gianluigi "Zane" Zanettini <ZaneCEO@users.noreply.github.com>
2023-01-21 16:47:43 +0100

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -4,6 +4,4 @@ echo ""
 ## Script name
 SCRIPT_NAME="zzmysqldump"
 
-source "/usr/local/turbolab.it/zzmysqldump/base.sh"
 bash "/usr/local/turbolab.it/zzmysqldump/zzmysqldump.sh" > "${MYSQL_BACKUP_DIR}automatic-backup.log" 2>&1
-
